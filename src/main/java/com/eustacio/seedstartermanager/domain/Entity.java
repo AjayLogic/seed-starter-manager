8d012edfab1286a971ff7b018c671db90a9aebf9
Eustácio <wallison.gth@gmail.com>
2017-08-11 18:39:44 -0300

    Added base class for all domain entities

---
+++ src/main/java/com/eustacio/seedstartermanager/domain/Entity.java
@@ -0,0 +1,24 @@
+package com.eustacio.seedstartermanager.domain;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * @author Wallison Freitas
+ */
+@MappedSuperclass
+public abstract class Entity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+}
