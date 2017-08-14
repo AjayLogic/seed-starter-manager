0aef9bcae3bc7d80c27d4ad4992b171be7a5782e
Eustácio <wallison.gth@gmail.com>
2017-08-14 20:06:43 -0300

    Fixed SeedStarterTest#equalsAndHashCodeContract() broked test method

--- src/test/java/com/eustacio/seedstartermanager/domain/SeedStarterTest.java
+++ src/test/java/com/eustacio/seedstartermanager/domain/SeedStarterTest.java
@@ -17,6 +17,13 @@ class SeedStarterTest {
         EqualsVerifier.forClass(SeedStarter.class)
                 // Two new objects are never equal to each other
                 .suppress(Warning.IDENTICAL_COPY_FOR_VERSIONED_ENTITY)
+
+                // Used to avoid Recursive datastructure assertion error.
+                // More info on http://jqno.nl/equalsverifier/errormessages/recursive-datastructure
+                .withPrefabValues(Row.class,
+                        new Row(new SeedVariety("Aldrovanda Vesiculosa"), 2),
+                        new Row(new SeedVariety("Amorphophallus titanum"), 3))
+
                 .verify();
     }
 
