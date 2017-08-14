5d1e5a0300b0cff47a724daa5784a6c3ddb785db
Eustácio <wallison.gth@gmail.com>
2017-08-14 20:04:41 -0300

    Fixed up the RowTest#equalsAndHashCodeContract() broken test method

--- src/test/java/com/eustacio/seedstartermanager/domain/RowTest.java
+++ src/test/java/com/eustacio/seedstartermanager/domain/RowTest.java
@@ -3,6 +3,8 @@ package com.eustacio.seedstartermanager.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 
@@ -17,6 +19,13 @@ class RowTest {
         EqualsVerifier.forClass(Row.class)
                 // Two new objects are never equal to each other
                 .suppress(Warning.IDENTICAL_COPY_FOR_VERSIONED_ENTITY)
+
+                // Used to avoid Recursive datastructure assertion error.
+                // More info on http://jqno.nl/equalsverifier/errormessages/recursive-datastructure
+                .withPrefabValues(SeedStarter.class,
+                        new SeedStarter(new MaterialType("Adamantium"), LocalDate.now(), false),
+                        new SeedStarter(new MaterialType("Vibranium"), LocalDate.now(), true))
+
                 .verify();
     }
 
