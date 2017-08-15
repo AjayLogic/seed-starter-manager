32dc6f245f1c0171abe5a7695f78db0472f95e91
Eustácio <wallison.gth@gmail.com>
2017-08-15 16:42:14 -0300

    Added repository interface to provide specific CRUD operations for the Row domain entity

---
+++ src/main/java/com/eustacio/seedstartermanager/repository/RowRepository.java
@@ -0,0 +1,10 @@
+package com.eustacio.seedstartermanager.repository;
+
+import com.eustacio.seedstartermanager.domain.Row;
+
+/**
+ * @author Wallison Freitas
+ */
+public interface RowRepository extends EntityRepository<Row> {
+
+}
