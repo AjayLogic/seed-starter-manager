72bec1f9cfd09fe1de0ad72808e2e6f894ad7a70
Eustácio <wallison.gth@gmail.com>
2017-08-15 16:48:14 -0300

    Added repository interface to provide specific CRUD operations for the SeedStarter domain entity

---
+++ src/main/java/com/eustacio/seedstartermanager/repository/SeedStarterRepository.java
@@ -0,0 +1,10 @@
+package com.eustacio.seedstartermanager.repository;
+
+import com.eustacio.seedstartermanager.domain.SeedStarter;
+
+/**
+ * @author Wallison Freitas
+ */
+public interface SeedStarterRepository extends EntityRepository<SeedStarter> {
+
+}
