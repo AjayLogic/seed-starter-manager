ba98ec51f163e2ea826a2acf2283718774d53e07
Eustácio <wallison.gth@gmail.com>
2017-08-15 14:07:23 -0300

    Added repository interface to provide specific CRUD operations for the SeedVariety domain entity

---
+++ src/main/java/com/eustacio/seedstartermanager/repository/SeedVarietyRepository.java
@@ -0,0 +1,10 @@
+package com.eustacio.seedstartermanager.repository;
+
+import com.eustacio.seedstartermanager.domain.SeedVariety;
+
+/**
+ * @author Wallison Freitas
+ */
+public interface SeedVarietyRepository extends NamedEntityRepository<SeedVariety> {
+
+}
