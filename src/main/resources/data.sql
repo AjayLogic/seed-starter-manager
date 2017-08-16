ae073c2a14b1e8b516305e777dea9efe96f28f7e
Eustácio <wallison.gth@gmail.com>
2017-08-16 14:29:13 -0300

    Added seed starter default values for the material type, feature and seed variety

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO MATERIAL_TYPE (ID, NAME)
+VALUES (1, 'Plastic'), (2, 'Wood'), (3, 'Iron'), (4, 'Aluminum');
+
+INSERT INTO FEATURE (ID, NAME)
+VALUES (1, 'Specific Substrate'), (2, 'Fertilizer'), (3, 'Ph Corrector'), (4, 'Thermal Heating');
+
+INSERT INTO SEED_VARIETY (ID, NAME)
+VALUES (1, 'Thymus Caespititius'), (2, 'Thymus Camphoratus'), (3, 'Thymus Capitellatus'),
+  (4, 'Thymus Carnosus'), (5, 'Thymus Mastichina'), (6, 'Thymus Praecox'),
+  (7, 'Thymus Pulegioides'), (8, 'Thymus Villosus'), (9, 'Thymus Vulgaris'), (10, 'Thymus Zygis');
