d3708b290d46f510ac5b671ce0f4a2cac8631b06
Eustácio <wallison.gth@gmail.com>
2017-08-16 12:50:21 -0300

    Added Spring @Configuration class that set up the data source for the persistence layer

---
+++ src/main/java/com/eustacio/seedstartermanager/config/persistence/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package com.eustacio.seedstartermanager.config.persistence;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Wallison Freitas
+ */
+@Configuration
+public class DataSourceConfig {
+
+    @Bean("dataSource")
+    public DataSource developmentDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setName("embedded_database")
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding(StandardCharsets.UTF_8.name())
+                .build();
+    }
+
+}
