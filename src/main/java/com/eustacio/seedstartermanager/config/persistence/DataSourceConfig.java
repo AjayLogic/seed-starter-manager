a23360a878bf70dcffcd9b383a27d17718dc5108
Eustácio <wallison.gth@gmail.com>
2017-08-21 16:56:08 -0300

    Added a new DataSource to use specifically on integration tests

--- src/main/java/com/eustacio/seedstartermanager/config/persistence/DataSourceConfig.java
+++ src/main/java/com/eustacio/seedstartermanager/config/persistence/DataSourceConfig.java
@@ -2,6 +2,7 @@ package com.eustacio.seedstartermanager.config.persistence;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -16,12 +17,24 @@ import javax.sql.DataSource;
 public class DataSourceConfig {
 
     @Bean("dataSource")
+    @Profile("dev")
     public DataSource developmentDataSource() {
         return new EmbeddedDatabaseBuilder()
                 .setName("embedded_database")
                 .setType(EmbeddedDatabaseType.H2)
                 .setScriptEncoding(StandardCharsets.UTF_8.name())
                 .addDefaultScripts()
+                .build();
+    }
+
+    @Bean("dataSource")
+    @Profile("integration-test")
+    public DataSource integrationTestDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding(StandardCharsets.UTF_8.name())
+                .addScripts("schema.sql", "data-test.sql")
                 .build();
     }
 
