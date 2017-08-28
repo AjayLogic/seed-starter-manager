2095b2376b5f15b2f8cd1bbd39bd8ca4a022a755
Eustácio <wallison.gth@gmail.com>
2017-08-28 18:41:16 -0300

    Added @ComponentScan annotation to the WebApplicationContext class to perform the scan of web layer related beans

--- src/main/java/com/eustacio/seedstartermanager/config/web/WebApplicationContext.java
+++ src/main/java/com/eustacio/seedstartermanager/config/web/WebApplicationContext.java
@@ -1,5 +1,6 @@
 package com.eustacio.seedstartermanager.config.web;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,6 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = {
+        "com.eustacio.seedstartermanager.config.web",
+        "com.eustacio.seedstartermanager.web"
+})
 public class WebApplicationContext implements WebMvcConfigurer {
 
 }
