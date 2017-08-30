962cc11a8590a10433c5b944b58b7ab72d4aec90
Eustácio <wallison.gth@gmail.com>
2017-08-30 16:16:25 -0300

    Added the @UnitTest annotation to initialize and configure the Spring test web context, in addition to enabling the appropriate profile for unit tests

---
+++ src/test/java/com/eustacio/seedstartermanager/UnitTest.java
@@ -0,0 +1,22 @@
+package com.eustacio.seedstartermanager;
+
+import com.eustacio.seedstartermanager.config.web.WebApplicationContext;
+
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Wallison Freitas
+ */
+@ActiveProfiles("dev")
+@SpringJUnitWebConfig(classes = WebApplicationContext.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface UnitTest {
+
+}
