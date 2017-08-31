4797cd5b87014a851587b74223237a3b0e1c5cee
Eustácio <wallison.gth@gmail.com>
2017-08-31 19:50:50 -0300

    Added EntityNotFoundException to indicates that an entity cannot be found

---
+++ src/main/java/com/eustacio/seedstartermanager/exception/EntityNotFoundException.java
@@ -0,0 +1,16 @@
+package com.eustacio.seedstartermanager.exception;
+
+/**
+ * @author Wallison Freitas
+ */
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
