0a4d7acdf3186e223f021c1857fefeeec97e7fce
Eustácio <wallison.gth@gmail.com>
2017-09-01 17:19:05 -0300

    Added @RestController class that contains REST endpoints to be used to perform CRUD operations on the Feature domain entity

---
+++ src/main/java/com/eustacio/seedstartermanager/web/controller/FeatureController.java
@@ -0,0 +1,80 @@
+package com.eustacio.seedstartermanager.web.controller;
+
+import com.eustacio.seedstartermanager.domain.Feature;
+import com.eustacio.seedstartermanager.exception.EntityNotFoundException;
+import com.eustacio.seedstartermanager.service.FeatureService;
+import com.eustacio.seedstartermanager.web.exception.support.ValidationError;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import static com.eustacio.seedstartermanager.web.WebUtility.mapToFieldError;
+
+/**
+ * @author Wallison Freitas
+ */
+@RestController
+@RequestMapping(path = "feature", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+public class FeatureController {
+
+    private FeatureService featureService;
+
+    @Autowired
+    public FeatureController(FeatureService featureService) {
+        this.featureService = featureService;
+    }
+
+    @GetMapping
+    public ResponseEntity getAllFeatures() {
+        List<Feature> featureList = featureService.findAll();
+        if (featureList.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(featureList);
+    }
+
+    @PostMapping
+    public ResponseEntity createOrUpdateFeature(@Valid @RequestBody Feature feature,
+                                                Errors errors, UriComponentsBuilder uriBuilder) {
+        if (errors.hasErrors()) {
+            ValidationError validationError = new ValidationError(HttpStatus.BAD_REQUEST, mapToFieldError(errors));
+            return ResponseEntity.badRequest().body(validationError);
+        }
+
+        Feature newFeature = featureService.save(feature);
+        URI location = uriBuilder.path("/feature/{id}").build(newFeature.getId());
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity findFeatureById(@PathVariable("id") Long id) {
+        Feature feature = featureService.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Cannot find Feature with id " + id));
+
+        return ResponseEntity.ok(feature);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteFeature(@PathVariable("id") Long id) {
+        featureService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/test/java/com/eustacio/seedstartermanager/web/controller/FeatureControllerIT.java
@@ -0,0 +1,178 @@
+package com.eustacio.seedstartermanager.web.controller;
+
+import com.eustacio.seedstartermanager.WebIntegrationTest;
+import com.eustacio.seedstartermanager.domain.Feature;
+import com.eustacio.seedstartermanager.exception.EntityNotFoundException;
+import com.eustacio.seedstartermanager.service.FeatureService;
+import com.eustacio.seedstartermanager.web.exception.RestEndpointExceptionHandler;
+
+import org.assertj.core.util.Lists;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static com.eustacio.seedstartermanager.TestUtil.convertToJson;
+import static com.eustacio.seedstartermanager.TestUtil.setId;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * @author Wallison Freitas
+ */
+@WebIntegrationTest
+class FeatureControllerIT {
+
+    @Autowired
+    private FeatureService mockService;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+    private List<Feature> featureList;
+
+    @BeforeEach
+    void beforeEachTest() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+
+        Feature specificSubstrate = new Feature("Specific Substrate");
+        Feature fertilizer = new Feature("Fertilizer");
+        this.featureList = Lists.newArrayList(specificSubstrate, fertilizer);
+    }
+
+    @Test
+    void getAllFeatures() throws Exception {
+        when(mockService.findAll()).thenReturn(featureList);
+
+        mockMvc.perform(get("/feature").accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.length()", is(equalTo(featureList.size()))))
+                .andExpect(jsonPath("$.[0].name", is(equalTo(featureList.get(0).getName()))))
+                .andExpect(jsonPath("$.[1].name", is(equalTo(featureList.get(1).getName()))));
+    }
+
+    @Test
+    @DisplayName("getAllFeatures() should return status 'No Content' when has no Feature")
+    void getAllFeatures_ShouldReturnStatusNoContent_WhenHasNoFeature() throws Exception {
+        when(mockService.findAll()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/feature"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void createOrUpdateFeature() throws Exception {
+        Feature newFeature = new Feature("Invincibility");
+        String payload = convertToJson(newFeature);
+
+        Feature savedFeature = new Feature(newFeature.getName());
+        setId(666L, savedFeature);
+
+        String expectedLocation = "/feature/" + savedFeature.getId();
+
+        when(mockService.save(newFeature)).thenReturn(savedFeature);
+
+
+        mockMvc.perform(post("/feature").contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(payload))
+                .andExpect(status().isCreated())
+                .andExpect(header().string(HttpHeaders.LOCATION, Matchers.endsWith(expectedLocation)));
+    }
+
+    @Test
+    @DisplayName("createOrUpdateFeature() should validate the Feature")
+    void createOrUpdateFeature_ShouldValidateTheFeature() throws Exception {
+        Feature invalidFeature = new Feature("");
+        String payload = convertToJson(invalidFeature);
+
+        mockMvc.perform(post("/feature").contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(payload))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code", is(HttpStatus.BAD_REQUEST.value())))
+                .andExpect(jsonPath("$.reason", is(HttpStatus.BAD_REQUEST.name())))
+                .andExpect(jsonPath("$.fieldErrors.length()", is(greaterThanOrEqualTo(1))));
+    }
+
+    @Test
+    void deleteFeature() throws Exception {
+        mockMvc.perform(delete("/feature/{id}", 6L))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findFeatureById() throws Exception {
+        Long featureId = 640509040147L;
+        Feature feature = new Feature("immortality");
+        setId(featureId, feature);
+
+        when(mockService.findById(featureId)).thenReturn(Optional.of(feature));
+
+
+        mockMvc.perform(get("/feature/{id}", featureId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(equalTo(featureId))))
+                .andExpect(jsonPath("$.name", is(equalTo(feature.getName()))));
+    }
+
+    @Test
+    @DisplayName("findFeatureById() should return status 'Not Found' when the Feature not exists")
+    void findFeatureById_ShouldReturnStatusNotFound_WhenTheFeatureNotExists() throws Exception {
+        Long featureId = 69L;
+
+        mockMvc.perform(get("/feature/{id}", featureId))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.code", is(HttpStatus.NOT_FOUND.value())))
+                .andExpect(jsonPath("$.reason", is(HttpStatus.NOT_FOUND.name())))
+                .andExpect(jsonPath("$.error", is(not(blankOrNullString()))));
+    }
+
+    @Test
+    @DisplayName("deleteFeature() should return status 'Not Found' when the Feature not exists")
+    void deleteFeature_ShouldReturnStatusNotFound_WhenTheFeatureNotExists() throws Exception {
+        FeatureController mockController = mock(FeatureController.class);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(mockController)
+                .setControllerAdvice(new RestEndpointExceptionHandler())
+                .build();
+
+        Long featureId = 666L;
+        EntityNotFoundException notFoundException = new EntityNotFoundException("Entity not found");
+        when(mockController.deleteFeature(featureId)).thenThrow(notFoundException);
+
+
+        mockMvc.perform(delete("/feature/{id}", featureId))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.code", is(HttpStatus.NOT_FOUND.value())))
+                .andExpect(jsonPath("$.reason", is(HttpStatus.NOT_FOUND.name())))
+                .andExpect(jsonPath("$.error", is(equalTo(notFoundException.getMessage()))));
+    }
+
+
+    @Configuration
+    static class TestConfig {
+
+        @Bean
+        FeatureService mockFeatureService() {
+            return Mockito.mock(FeatureService.class);
+        }
+    }
+
+}
