17e91f350ce1a2869fb882bb4204eb572c9a9476
Eustácio <wallison.gth@gmail.com>
2017-09-12 18:40:28 -0300

    Added dropdown menu to the navbar to provides a way to change the application theme dynamically

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ThemeManagerService } from './core/theme-manager/theme-manager.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private themes: string[];
+
+  constructor(private themeManager: ThemeManagerService) {}
+
+  ngOnInit(): void {
+    this.themes = this.themeManager.getThemes();
+  }
+
+  private onThemeSelected(themeName: string): void {
+    this.themeManager.setTheme(themeName);
+  }
 
 }
