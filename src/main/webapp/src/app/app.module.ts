db157ec00b166144ccfe348623b400e956c47a45
Eustácio <wallison.gth@gmail.com>
2017-09-12 18:09:35 -0300

    Added the core.module to the app.module

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
 import { SeedVarietyModule } from './seed-variety/seed-variety.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import { SeedVarietyModule } from './seed-variety/seed-variety.module';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     HomeModule,
     SeedVarietyModule,
     AppRoutingModule
