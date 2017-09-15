77e9d4e073a4efcd9113032fb5253451d97f45f0
Eustácio <wallison.gth@gmail.com>
2017-09-15 15:34:07 -0300

    Added the app-simple-list selector tag to the feature.component be able to show all features in a list

--- src/main/webapp/src/app/feature/feature.component.ts
+++ src/main/webapp/src/app/feature/feature.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
+
+import { FeatureService } from './feature.service';
+import { Feature } from '../model/feature';
 
 @Component({
   selector: 'app-feature',
   templateUrl: './feature.component.html',
   styleUrls: ['./feature.component.css']
 })
-export class FeatureComponent {
+export class FeatureComponent implements OnInit, OnDestroy {
+
+  private features: Feature[];
+  private subject: Subject<void> = new Subject();
+
+  constructor(private service: FeatureService) {}
+
+  ngOnInit(): void {
+    this.fetchAllFeatures();
+  }
+
+  ngOnDestroy(): void {
+    this.subject.next();
+    this.subject.complete();
+  }
+
+  private fetchAllFeatures(): void {
+    this.service.getAllFeatures()
+      .takeUntil(this.subject)
+      .subscribe((features: Feature[]) => this.features = features);
+  }
+
 }
--- src/main/webapp/src/app/feature/feature.module.ts
+++ src/main/webapp/src/app/feature/feature.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { FeatureRoutingModule } from './feature-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { FeatureComponent } from './feature.component';
 
 import { FeatureService } from './feature.service';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [FeatureComponent],
   imports: [
     CommonModule,
     HttpClientModule,
-    FeatureRoutingModule
+    FeatureRoutingModule,
+    SharedModule
   ],
   providers: [FeatureService]
 })
