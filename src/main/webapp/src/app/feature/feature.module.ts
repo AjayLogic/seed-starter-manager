c68672c3c10a873e9329bdaac5263c9a35e007cb
Eustácio <wallison.gth@gmail.com>
2017-09-15 13:52:26 -0300

    Added method to fetch all features to the feature.service

--- src/main/webapp/src/app/feature/feature.module.ts
+++ src/main/webapp/src/app/feature/feature.module.ts
@@ -6,11 +6,13 @@ import { FeatureRoutingModule } from './feature-routing.module';
 import { FeatureComponent } from './feature.component';
 
 import { FeatureService } from './feature.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [FeatureComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     FeatureRoutingModule
   ],
   providers: [FeatureService]
--- src/main/webapp/src/app/feature/feature.service.ts
+++ src/main/webapp/src/app/feature/feature.service.ts
@@ -1,5 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Feature } from '../model/feature';
 
 @Injectable()
 export class FeatureService {
+
+  private readonly endpointUrl: string = '/api/feature';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllFeatures(): Observable<Feature[]> {
+    return this.httpClient.get<Feature[]>(`${this.endpointUrl}`);
+  }
+
 }
