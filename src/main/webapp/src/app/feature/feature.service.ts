ea5c8a67cec972b4f5844de21924aefb3d32927c
Eustácio <wallison.gth@gmail.com>
2017-09-16 11:03:21 -0300

    Added method to the feature.service to be able to add a new feature

--- src/main/webapp/src/app/feature/feature.service.ts
+++ src/main/webapp/src/app/feature/feature.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 import { Feature } from '../model/feature';
@@ -13,6 +13,14 @@ export class FeatureService {
 
   getAllFeatures(): Observable<Feature[]> {
     return this.httpClient.get<Feature[]>(`${this.endpointUrl}`);
+  }
+
+  addFeature(name: string): Observable<HttpResponse<any>> {
+    let header = new HttpHeaders({ ['Content-Type']: 'application/json' });
+    let payload = { name: name };
+
+    return this.httpClient
+      .post(`${this.endpointUrl}`, payload, { headers: header, observe: 'response' });
   }
 
 }
