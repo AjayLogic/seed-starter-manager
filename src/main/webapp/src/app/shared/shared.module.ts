7dac78e72fcbe55195063ed36c8b58ac7e60b368
Eustácio <wallison.gth@gmail.com>
2017-09-21 13:22:24 -0300

    Added error-label.directive to help to set error messages for input fields, and added it to the shared.module

--- src/main/webapp/src/app/shared/shared.module.ts
+++ src/main/webapp/src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SimpleListComponent } from './simple-list/simple-list.component';
+import { InputErrorLabelDirective } from './error-label/error-label.directive';
 
 @NgModule({
-  declarations: [SimpleListComponent],
+  declarations: [SimpleListComponent, InputErrorLabelDirective],
   imports: [CommonModule],
-  exports: [SimpleListComponent]
+  exports: [SimpleListComponent, InputErrorLabelDirective]
 })
 export class SharedModule {
 }
