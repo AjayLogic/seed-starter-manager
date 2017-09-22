8d6087199c829fc1d516c1afa0c97b0c4890e734
Eustácio <wallison.gth@gmail.com>
2017-09-22 13:07:49 -0300

    Added static factory method to create instances of ServiceError based on the status code of the http response

--- src/main/webapp/src/app/model/service-error.ts
+++ src/main/webapp/src/app/model/service-error.ts
@@ -5,4 +5,16 @@ export class ServiceError {
   constructor(readonly type: ErrorType,
               readonly message?: string) { }
 
+  public static fromStatusCode(status: number): ServiceError | null {
+    switch (status) {
+      case 204:  // No Content
+        return new ServiceError(ErrorType.EMPTY, 'The requested resource is empty');
+      case 504:  // Gateway Timeout
+        return new ServiceError(ErrorType.GATEWAY_TIMEOUT, 'The server is probably is down or cannot be reached');
+      default:
+        console.log('Cannot match any error type for the status code: ', status);
+        return null;
+    }
+  }
+
 }
