3454354df4bd77fee265006aafe9d9038d2be7a4
Eustácio <wallison.gth@gmail.com>
2017-09-22 13:15:22 -0300

    Refactored the loadInitialData() method of feature.service to make use of the ServiceError factory method when is needed to publish a error for the error subscribers

--- src/main/webapp/src/app/feature/feature.service.ts
+++ src/main/webapp/src/app/feature/feature.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+  HttpResponse,
+  HttpResponseBase
+} from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 
 import { Feature } from '../model/feature';
 import { ServiceError } from '../model/service-error';
-import { ErrorType } from '../model/error-type.enum';
 
 @Injectable()
 export class FeatureService {
@@ -40,27 +45,15 @@ export class FeatureService {
   private loadInitialData(): void {
     this.httpClient.get<Feature[]>(`${this.endpointUrl}`, { observe: 'response' })
       .subscribe((response: HttpResponse<Feature[]>) => {
-          switch (response.status) {
-            case 200:  // Ok
-              this.featuresSubject.next(response.body);
-              break;
-            case 204:  // No content
-              this.errorSubject.next(new ServiceError(ErrorType.EMPTY, 'No features can be found'));
-              break;
-            default:
-              console.log('Cannot match any error type for the response: ', response);
-          }
+          if (response.status == 200) this.featuresSubject.next(response.body);
+          else this.publishError(response);
         },
-        (error: HttpErrorResponse) => {
-          switch (error.status) {
-            case 504:  // Gateway Timeout
-              this.errorSubject.next(new ServiceError(ErrorType.GATEWAY_TIMEOUT, 'The server is probably is down or cannot be reached'));
-              break;
-            default:
-              console.log('Cannot match any error type for the error: ', error);
-          }
-        }
+        (error: HttpErrorResponse) => this.publishError(error)
       );
+  }
+
+  private publishError(response: HttpResponseBase) {
+    this.errorSubject.next(ServiceError.fromStatusCode(response.status));
   }
 
 }
