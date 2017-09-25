545cff5340560cb2987083ef45037cf76fca2753
Eustácio <wallison.gth@gmail.com>
2017-09-25 16:55:04 -0300

    Refactored the createOrUpdateFeature() method of feature.service to add the proper handling when an Feature is updated

--- src/main/webapp/src/app/feature/feature.service.ts
+++ src/main/webapp/src/app/feature/feature.service.ts
@@ -36,11 +36,15 @@ export class FeatureService {
 
     this.httpClient.post(`${this.endpointUrl}`, feature, { headers: header, observe: 'response' })
       .subscribe((response: HttpResponse<Feature>) => {
-          if (response.status == 201) {  // Created
-            let savedFeature = response.body;
-            this.featuresSubject.next(this.featuresSubject.getValue().concat(savedFeature));
-          } else {
-            this.publishError(response);
+          switch (response.status) {
+            case 200:  // Ok (Feature has been updated successfully)
+              this.onFeatureUpdated(feature);
+              break;
+            case 201:  // Created (Feature has been created successfully)
+              this.onFeatureCreated(response.body);
+              break;
+            default:
+              this.publishError(response);
           }
         },
         (error: HttpErrorResponse) => this.publishError(error)
@@ -55,6 +59,22 @@ export class FeatureService {
         },
         (error: HttpErrorResponse) => this.publishError(error)
       );
+  }
+
+  private onFeatureUpdated(updatedFeature: Feature) {
+    this.featuresSubject.getValue().find((existentFeature: Feature) => {
+      // Updates the name of the corresponding Feature in the current list, and publish it
+      if (existentFeature.id == updatedFeature.id) {
+        existentFeature.name = updatedFeature.name;
+        this.featuresSubject.next(this.featuresSubject.getValue());
+        return true;
+      }
+    });
+  }
+
+  private onFeatureCreated(newFeature: Feature) {
+    let features: Feature[] = this.featuresSubject.getValue();
+    this.featuresSubject.next(features.concat(newFeature));
   }
 
   private publishError(response: HttpResponseBase) {
