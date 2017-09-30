e84ada09c0a69fee6d77798c7f3e7e5426eaba24
Eustácio <wallison.gth@gmail.com>
2017-09-30 15:33:06 -0300

    Added method to load all material types when the material-type.component starts and the app-simple-list.component to display them on the template

--- src/main/webapp/src/app/material-type/material-type.component.ts
+++ src/main/webapp/src/app/material-type/material-type.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+import { MaterialTypeService } from './material-type.service';
+import { MaterialType } from '../model/material-type';
 
 @Component({
   selector: 'app-material-type',
   templateUrl: './material-type.component.html',
   styleUrls: ['./material-type.component.css']
 })
-export class MaterialTypeComponent {
+export class MaterialTypeComponent implements OnInit, OnDestroy {
+
+  materials: MaterialType[];
+
+  private subject: Subject<void> = new Subject();
+
+  constructor(private materialTypeService: MaterialTypeService) {}
+
+  ngOnInit(): void {
+    this.fetchAllMaterialTypes();
+  }
+
+  ngOnDestroy(): void {
+    this.subject.next();
+    this.subject.complete();
+  }
+
+  get hasMaterials(): boolean {
+    return Array.isArray(this.materials) && this.materials.length > 0;
+  }
+
+  private fetchAllMaterialTypes(): void {
+    this.materialTypeService.materials
+      .takeUntil(this.subject)
+      .subscribe((materials: MaterialType[]) => this.materials = materials);
+  }
 
 }
--- src/main/webapp/src/app/material-type/material-type.module.ts
+++ src/main/webapp/src/app/material-type/material-type.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MaterialTypeRoutingModule } from './material-type-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { MaterialTypeComponent } from './material-type.component';
 
@@ -11,7 +12,8 @@ import { MaterialTypeService } from './material-type.service';
   declarations: [MaterialTypeComponent],
   imports: [
     CommonModule,
-    MaterialTypeRoutingModule
+    MaterialTypeRoutingModule,
+    SharedModule
   ],
   providers: [MaterialTypeService]
 })
