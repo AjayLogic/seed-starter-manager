49b5fa3e8c4aa9c6c82eafbb0fe3077ccd0d8ba4
Eustácio <wallison.gth@gmail.com>
2017-09-30 16:25:58 -0300

    Added floating button to allow add a new material type and the dialog that allows enter the material name

--- src/main/webapp/src/app/material-type/material-type.component.ts
+++ src/main/webapp/src/app/material-type/material-type.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, ValidationErrors, Validators } from '@angular/forms';
 import { Subject } from 'rxjs/Subject';
 
 import { MaterialTypeService } from './material-type.service';
@@ -13,8 +14,11 @@ import { ErrorType } from '../model/error-type.enum';
 })
 export class MaterialTypeComponent implements OnInit, OnDestroy {
 
+  inputName: FormControl;
+
   materials: MaterialType[];
 
+  private readonly maxMaterialName = 50; // TODO: fetch this information from database
   private subject: Subject<void> = new Subject();
 
   constructor(private materialTypeService: MaterialTypeService) {}
@@ -22,6 +26,7 @@ export class MaterialTypeComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.fetchAllMaterialTypes();
     this.registerForErrors();
+    this.initializeFormControls();
   }
 
   ngOnDestroy(): void {
@@ -31,6 +36,19 @@ export class MaterialTypeComponent implements OnInit, OnDestroy {
 
   get hasMaterials(): boolean {
     return Array.isArray(this.materials) && this.materials.length > 0;
+  }
+
+  get errorMessages(): any {
+    return {
+      required: 'The material must have a name',
+      maxlength: 'The material name must have less than ' + this.maxMaterialName + ' characters',
+      conflict: 'This material already exists'
+    };
+  }
+
+  getInputFieldClass(input: FormControl): string {
+    return input.valid ? 'valid' :
+      input.invalid && input.touched || input.invalid && input.dirty ? 'invalid' : '';
   }
 
   private fetchAllMaterialTypes(): void {
@@ -56,6 +74,21 @@ export class MaterialTypeComponent implements OnInit, OnDestroy {
           }
         }
       });
+  }
+
+  private initializeFormControls(): void {
+    this.inputName = new FormControl('', [
+      Validators.required, Validators.maxLength(this.maxMaterialName), this.uniqueMaterialType.bind(this)
+    ]);
+  }
+
+  private uniqueMaterialType(formControl: AbstractControl): ValidationErrors {
+    const currentMaterialName = formControl.value;
+    let isMaterialDuplicated = this.materials.some((material: MaterialType) => {
+      return formControl.dirty && material.name == currentMaterialName;
+    });
+
+    return isMaterialDuplicated ? { conflict: true } : null;
   }
 
 }
--- src/main/webapp/src/app/material-type/material-type.module.ts
+++ src/main/webapp/src/app/material-type/material-type.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialTypeRoutingModule } from './material-type-routing.module';
 import { SharedModule } from '../shared/shared.module';
@@ -12,6 +13,7 @@ import { MaterialTypeService } from './material-type.service';
   declarations: [MaterialTypeComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     MaterialTypeRoutingModule,
     SharedModule
   ],
