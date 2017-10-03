a5e93b497436030a6224080eb1ed985947e16b13
Eustácio <wallison.gth@gmail.com>
2017-10-03 17:25:44 -0300

    Added image field to the SeedVariety class and to the database schema

--- src/main/java/com/eustacio/seedstartermanager/domain/SeedVariety.java
+++ src/main/java/com/eustacio/seedstartermanager/domain/SeedVariety.java
@@ -1,5 +1,6 @@
 package com.eustacio.seedstartermanager.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -10,12 +11,23 @@ import javax.persistence.Table;
 @Table(name = "SEED_VARIETY")
 public class SeedVariety extends NamedEntity {
 
+    @Column(name = "IMAGE")
+    private byte[] image;
+
     protected SeedVariety() {
         // Constructor without arguments to agree with the JPA specification
     }
 
     public SeedVariety(String name) {
         super(name);
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
     }
 
 }
