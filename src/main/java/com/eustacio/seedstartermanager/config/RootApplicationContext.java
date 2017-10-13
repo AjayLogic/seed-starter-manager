cd071bbfaf535423cf185842ef401c6667cb6ba5
Eustácio <wallison.gth@gmail.com>
2017-10-13 17:58:07 -0300

    Added Spring annotation @PropertySource to add the application.properties file to the Spring Environment, and added the @Bean 'PropertySourcesPlaceholderConfigurer' to resolves the ${...} placeholders within bean definition property values and @Value annotations against the current Spring Environment and its set of PropertySources

--- src/main/java/com/eustacio/seedstartermanager/config/RootApplicationContext.java
+++ src/main/java/com/eustacio/seedstartermanager/config/RootApplicationContext.java
@@ -1,13 +1,17 @@
 package com.eustacio.seedstartermanager.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  * @author Wallison Freitas
  */
 @Configuration
+@PropertySource("classpath:application.properties")
 @ComponentScan(value = "com.eustacio.seedstartermanager", excludeFilters = {
         @ComponentScan.Filter(type = FilterType.REGEX, pattern = {
                 // Let the web context scan your own beans
@@ -16,5 +20,10 @@ import org.springframework.context.annotation.FilterType;
         })
 })
 public class RootApplicationContext {
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
 }
