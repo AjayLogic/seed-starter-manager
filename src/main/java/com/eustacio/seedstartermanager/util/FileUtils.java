a112c83761cc6253103bbc09067d370e1c88b9c5
Eustácio <wallison.gth@gmail.com>
2017-10-14 13:41:58 -0300

    Added method to the FileUtils class to get a java.util.Properties from a file

--- src/main/java/com/eustacio/seedstartermanager/util/FileUtils.java
+++ src/main/java/com/eustacio/seedstartermanager/util/FileUtils.java
@@ -1,8 +1,24 @@
 package com.eustacio.seedstartermanager.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * @author Wallison.
  */
 public abstract class FileUtils {
+
+    public static Properties getPropertiesFromFile(String fileName) {
+        Properties properties = new Properties();
+
+        try (InputStream inputStream = FileUtils.class.getClassLoader().getResourceAsStream(fileName)) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
 
 }
