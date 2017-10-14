6624e34fca882469409e8333120f79801c01fc10
Eustácio <wallison.gth@gmail.com>
2017-10-14 16:07:59 -0300

    Added @Bean StandardServletMultipartResolver, to resolve requests with the multipart/form-data MIME type

--- src/main/java/com/eustacio/seedstartermanager/config/web/WebApplicationContext.java
+++ src/main/java/com/eustacio/seedstartermanager/config/web/WebApplicationContext.java
@@ -1,7 +1,10 @@
 package com.eustacio.seedstartermanager.config.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +18,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "com.eustacio.seedstartermanager.web"
 })
 public class WebApplicationContext implements WebMvcConfigurer {
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
 
 }
