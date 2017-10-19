adafbe2102c893754ffc368f81397f48ff4b385e
Eustácio <wallison.gth@gmail.com>
2017-10-19 16:21:31 -0200

    Added 'getAllAllowedFileExtensions' method to the StandardServerStorageManager class to be able to get all file extensions allowed by the server and your folder location to upload

--- src/main/java/com/eustacio/seedstartermanager/web/storageManager/StandardServerStorageManager.java
+++ src/main/java/com/eustacio/seedstartermanager/web/storageManager/StandardServerStorageManager.java
@@ -1,16 +1,24 @@
 package com.eustacio.seedstartermanager.web.storageManager;
 
+import com.eustacio.seedstartermanager.util.PropertiesUtils;
+import com.sun.istack.internal.NotNull;
+
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Properties;
 
 /**
  * @author Wallison S. Freitas
  */
 @Component
 public class StandardServerStorageManager implements ServerStorageManager {
+
+    private Hashtable<String, String> allowedExtensions;
 
     @Override
     public File transferFileToServer(MultipartFile file, String newFileName) {
@@ -28,6 +36,39 @@ public class StandardServerStorageManager implements ServerStorageManager {
 
         // Returns null if the 'fileName' parameter is null, empty or contains only white spaces
         return null;
+    }
+
+    @NotNull
+    protected Hashtable<String, String> getAllAllowedFileExtensions() {
+        if (allowedExtensions == null) {
+            allowedExtensions = new Hashtable<>();
+
+            Properties applicationProperties = PropertiesUtils.getApplicationProperties();
+            applicationProperties.forEach((key, value) -> {
+                String propertyKey = key.toString();
+                if (propertyKey.startsWith("server.upload.allowed_extensions.")) {
+                    String[] extensions = splitStringOnAnySpecialCharacter(value.toString());
+                    String fileType = propertyKey.substring(propertyKey.lastIndexOf('.'));
+                    String location = applicationProperties.getProperty("server.upload.location" + fileType);
+                    if (location == null) {
+                        throw new IllegalStateException(String.format("Cannot find the appropriate " +
+                                        "folder to save files of type(s) %s, did you remember of provides " +
+                                        "an property named '%s' on the 'application.properties' file?",
+                                Arrays.toString(extensions), "server.upload.location" + fileType));
+                    }
+
+                    for (String extension : extensions) {
+                        allowedExtensions.put(extension, location);
+                    }
+                }
+            });
+        }
+
+        return allowedExtensions;
+    }
+
+    protected String[] splitStringOnAnySpecialCharacter(String str) {
+        return str.split("\\W+");
     }
 
 }
