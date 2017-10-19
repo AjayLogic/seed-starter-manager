0378b362edbb28b6805a3786426cedaf23ff0ee4
Eustácio <wallison.gth@gmail.com>
2017-10-19 13:06:46 -0200

    Added method to the StandardServerStorageManager class to sanitize an String by changing all characters to lower case, replacing empty spaces by underline and removing any character that is not a letter or a number

--- src/main/java/com/eustacio/seedstartermanager/web/storageManager/StandardServerStorageManager.java
+++ src/main/java/com/eustacio/seedstartermanager/web/storageManager/StandardServerStorageManager.java
@@ -1,6 +1,7 @@
 package com.eustacio.seedstartermanager.web.storageManager;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -13,6 +14,19 @@ public class StandardServerStorageManager implements ServerStorageManager {
 
     @Override
     public File transferFileToServer(MultipartFile file, String newFileName) {
+        return null;
+    }
+
+    protected String sanitizeFileName(String fileName) {
+        if (StringUtils.hasText(fileName)) {
+            return fileName
+                    .toLowerCase()            // Change all characters to lowercase
+                    .trim()                   // Removes any empty space on the start or end of the String
+                    .replaceAll(" +", "_")    // Replaces all empty spaces with a underscore
+                    .replaceAll("\\W+", "");  // Removes all characters that is not a-z, A-Z, 0-9 or _
+        }
+
+        // Returns null if the 'fileName' parameter is null, empty or contains only white spaces
         return null;
     }
 
