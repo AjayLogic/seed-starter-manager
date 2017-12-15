816cb093ac803f26ad200847e624cfe34704af9a
Eustácio <wallison.gth@gmail.com>
2017-12-15 09:22:08 -0200

    Added seed-starter.service class to provides CRUD operations for the seed starter

--- src/main/webapp/src/app/seed-starter/seed-starter.module.ts
+++ src/main/webapp/src/app/seed-starter/seed-starter.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SeedStarterComponent } from './seed-starter.component';
+import { SeedStarterService } from './seed-starter.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SeedStarterComponent]
+  declarations: [SeedStarterComponent],
+  providers: [SeedStarterService]
 })
 export class SeedStarterModule {}
