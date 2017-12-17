75df230bb18ea439d804adf8ea2163a2ff9cb0fb
Eustácio <wallison.gth@gmail.com>
2017-12-17 07:12:50 -0200

    Added the "MaterializeModule" to the "imports" section of the seed-starter.module, to be able to use the angular2-materialize on the form.component

--- src/main/webapp/src/app/seed-starter/seed-starter.module.ts
+++ src/main/webapp/src/app/seed-starter/seed-starter.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { SeedStarterComponent } from './seed-starter.component';
 import { SeedStarterService } from './seed-starter.service';
@@ -9,6 +10,7 @@ import { FormComponent } from './form/form.component';
 @NgModule({
   imports: [
     CommonModule,
+    MaterializeModule,
     SeedStarterRoutingModule
   ],
   declarations: [
