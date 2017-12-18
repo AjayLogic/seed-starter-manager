d082328c126800b1ffce126c4d7e1a521230e412
Eustácio <wallison.gth@gmail.com>
2017-12-18 08:38:41 -0200

    Moved the FeatureModule, MaterialTypeModule and the SeedVarietyModule from the app.module to the seed-starter.module

--- src/main/webapp/src/app/seed-starter/seed-starter.module.ts
+++ src/main/webapp/src/app/seed-starter/seed-starter.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterializeModule } from 'angular2-materialize';
 
-import { SeedStarterComponent } from './seed-starter.component';
-import { SeedStarterService } from './seed-starter.service';
+import { FeatureModule } from '../feature/feature.module';
+import { MaterialTypeModule } from '../material-type/material-type.module';
+import { SeedVarietyModule } from '../seed-variety/seed-variety.module';
 import { SeedStarterRoutingModule } from './seed-starter-routing.module';
+
+import { SeedStarterService } from './seed-starter.service';
+
+import { SeedStarterComponent } from './seed-starter.component';
 import { FormComponent } from './form/form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterializeModule,
+    FeatureModule,
+    MaterialTypeModule,
+    SeedVarietyModule,
     SeedStarterRoutingModule
   ],
   declarations: [
