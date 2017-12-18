dae8c463f5c988297372d8f4c0df628ca76f08fe
Eustácio <wallison.gth@gmail.com>
2017-12-18 08:40:34 -0200

    Added the ReactiveFormsModule to the "imports" section of the seed-starter.module

--- src/main/webapp/src/app/seed-starter/seed-starter.module.ts
+++ src/main/webapp/src/app/seed-starter/seed-starter.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterializeModule } from 'angular2-materialize';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { FeatureModule } from '../feature/feature.module';
 import { MaterialTypeModule } from '../material-type/material-type.module';
@@ -16,6 +17,7 @@ import { FormComponent } from './form/form.component';
   imports: [
     CommonModule,
     MaterializeModule,
+    ReactiveFormsModule,
     FeatureModule,
     MaterialTypeModule,
     SeedVarietyModule,
