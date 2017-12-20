a10ee825dbad6bb0db359c58351130e580f9c822
Eustácio <wallison.gth@gmail.com>
2017-12-20 16:22:21 -0200

    Added input-with-validation.directive to replace the error-label.directive in a near future due to the verbosity of the same, and added a new folder to store specifically directives

---
+++ src/main/webapp/src/app/shared/directives/input-with-validation/input-with-validation.directive.ts
@@ -0,0 +1,77 @@
+import { Directive, ElementRef, Input, OnChanges, OnInit, Renderer2, SimpleChanges } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Directive({
+  selector: '[appInputWithValidation]'
+})
+export class InputWithValidationDirective implements OnInit, OnChanges {
+
+  @Input() control: AbstractControl;
+  @Input() errorMessages: { [error: string]: string };
+
+  private inputLabel: Element;
+
+  constructor(private inputElement: ElementRef, private renderer: Renderer2) {
+    this.inputLabel = this.findLabelForInput(inputElement);
+  }
+
+  ngOnInit(): void {
+    if (!this.control) throw new Error(`Attribute 'control' is required!`);
+    if (!this.errorMessages) throw new Error(`Attribute 'errorMessages' is required!`);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.updateInputClass();
+    this.updateErrorLabel();
+  }
+
+  private updateInputClass(): void {
+    const inputClass = this.control.valid ? 'valid' :
+      this.control.invalid && (this.control.touched || this.control.dirty) ? 'invalid' : '';
+
+    if (inputClass == 'valid') {
+      this.changeClass('invalid', 'valid', this.inputElement);
+    } else if (inputClass == 'invalid') {
+      this.changeClass('valid', 'invalid', this.inputElement);
+    }
+  }
+
+  private updateErrorLabel(): void {
+    let errorMessage = '';
+    const inputErrors = this.control.errors;
+
+    if (inputErrors) {
+      // Search the error message based on the current error of the input
+      Object.keys(inputErrors).find((error: string) => {
+        if (this.errorMessages[error]) {
+          errorMessage = this.errorMessages[error];
+          return true;
+        }
+      });
+    }
+
+    if (this.inputElement.nativeElement.classList.contains('invalid')) {
+      this.renderer.setAttribute(this.inputLabel, 'data-error', errorMessage);
+    } else {
+      this.renderer.removeAttribute(this.inputLabel, 'data-error');
+    }
+  }
+
+  private findLabelForInput(inputElement: ElementRef): Element {
+    const elementId = inputElement.nativeElement.id;
+    const label = inputElement.nativeElement.parentElement.querySelector(`label[for="${elementId}"]`);
+
+    if (!label) {
+      throw new Error(`The element with id '${elementId}' must have a ` +
+        `correspondent label with the attribute 'for' specified!`);
+    }
+
+    return label;
+  }
+
+  private changeClass(from: string, to: string, element: ElementRef): void {
+    this.renderer.removeClass(element.nativeElement, from);
+    this.renderer.addClass(element.nativeElement, to);
+  }
+
+}
