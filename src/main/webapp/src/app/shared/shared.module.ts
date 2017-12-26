b3982bce3b92b0097f9cfb4a07edc991b991c082
Eustácio <wallison.gth@gmail.com>
2017-12-26 05:52:41 -0200

    Moved the simple-list.component to the 'ui' folder, and updated your import on the shared.module

--- src/main/webapp/src/app/shared/shared.module.ts
+++ src/main/webapp/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterializeModule } from 'angular2-materialize';
 
-import { SimpleListComponent } from './simple-list/simple-list.component';
+import { SimpleListComponent } from './ui/simple-list/simple-list.component';
 import { SimpleDialogComponent } from './simple-dialog/simple-dialog.component';
 import { LoadingComponent } from './ui/loading/loading.component';
 
