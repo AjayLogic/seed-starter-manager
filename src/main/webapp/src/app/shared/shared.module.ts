fb15fb59c4e6fe1cf1fcf5dd88a95bfe1666deb3
Eustácio <wallison.gth@gmail.com>
2017-12-26 05:59:30 -0200

    Moved the simple-dialog.component to the 'ui' folder, and updated your import on the shared.module

--- src/main/webapp/src/app/shared/shared.module.ts
+++ src/main/webapp/src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MaterializeModule } from 'angular2-materialize';
 
 import { SimpleListComponent } from './ui/simple-list/simple-list.component';
-import { SimpleDialogComponent } from './simple-dialog/simple-dialog.component';
+import { SimpleDialogComponent } from './ui/simple-dialog/simple-dialog.component';
 import { LoadingComponent } from './ui/loading/loading.component';
 
 import { InputErrorLabelDirective } from './error-label/error-label.directive';
