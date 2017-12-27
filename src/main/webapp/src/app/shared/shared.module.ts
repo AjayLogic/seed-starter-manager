17ddd39838865f1ef2e3b2e83b3611194ee934d0
Eustácio <wallison.gth@gmail.com>
2017-12-27 09:17:07 -0200

    Added the toast.service to the shared.module, to store the logic of the Materialize toast messages in a single place

--- src/main/webapp/src/app/shared/shared.module.ts
+++ src/main/webapp/src/app/shared/shared.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterializeModule } from 'angular2-materialize';
 
+import { ToastService } from './ui/toast-service/toast.service';
+
 import { SimpleListComponent } from './ui/simple-list/simple-list.component';
 import { SimpleDialogComponent } from './ui/simple-dialog/simple-dialog.component';
 import { LoadingComponent } from './ui/loading/loading.component';
@@ -24,7 +26,8 @@ import { InputWithValidationDirective } from './directives/input-with-validation
     SimpleDialogComponent,
     LoadingComponent,
     InputWithValidationDirective
-  ]
+  ],
+  providers: [ToastService]
 })
 export class SharedModule {
 }
