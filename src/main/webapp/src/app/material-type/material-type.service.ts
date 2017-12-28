13102951a15f117f2d6ab55fe613d680ff49f622
Eustácio <wallison.gth@gmail.com>
2017-12-28 18:53:31 -0200

    Refactored the material-type.service to the new service class hierarchy and changed all your usages. This completes a goal for the issue #16

--- src/main/webapp/src/app/material-type/material-type.service.ts
+++ src/main/webapp/src/app/material-type/material-type.service.ts
@@ -1,100 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse, HttpResponseBase } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
-import { ServiceError } from '../model/service-error';
 import { MaterialType } from '../model/material-type';
+import { EntityService } from '../service/entity-service';
 
 @Injectable()
-export class MaterialTypeService {
-
-  private readonly endpointUrl: string = '/api/material';
-  private httpHeader: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
-  private materialsSubject: BehaviorSubject<MaterialType[]> = new BehaviorSubject([]);
-  private errorSubject: BehaviorSubject<ServiceError> = new BehaviorSubject(null);
+export class MaterialTypeService extends EntityService<MaterialType> {
 
   constructor(private httpClient: HttpClient) {
-    this.loadInitialData();
+    super('/api/material');
+    super.loadData(httpClient);
+  }
+
+  save(material: MaterialType): void {
+    // Sends the new material to the server, and then uses the superclass method to handle the response
+    const response = this.httpClient.post<MaterialType>(`${this.endpointUrl}`, material,
+      { headers: this.jsonHttpHeader, observe: 'response' });
+
+    super.handleSaveResponse(response);
+  }
+
+  delete(material: MaterialType): void {
+    // Sends the delete request to the server, and then uses the superclass method to handle the response
+    const response = this.httpClient.delete<MaterialType>(`${this.endpointUrl}/${material.id}`,
+      { headers: this.jsonHttpHeader, observe: 'response' });
+
+    super.handleDeleteResponse(material, response);
   }
 
   get materials(): Observable<MaterialType[]> {
-    return this.materialsSubject.asObservable();
-  }
-
-  get errors(): Observable<ServiceError> {
-    return this.errorSubject.asObservable();
-  }
-
-  createOrUpdateMaterial(material: MaterialType): void {
-    this.httpClient.post(`${this.endpointUrl}`, material, { headers: this.httpHeader, observe: 'response' })
-      .subscribe((response: HttpResponse<MaterialType>) => {
-          switch (response.status) {
-            case 200:  // Ok (Material Type has been updated successfully)
-              this.onMaterialUpdated(material);
-              break;
-            case 201:  // Created (MaterialType has been created successfully)
-              this.onMaterialCreated(response.body);
-              break;
-            default:
-              this.publishError(response);
-          }
-        },
-        (error: HttpErrorResponse) => this.publishError(error)
-      );
-  }
-
-  deleteMaterial(material: MaterialType): void {
-    this.httpClient.delete(`${this.endpointUrl}/${material.id}`, { headers: this.httpHeader, observe: 'response' })
-      .subscribe((response: HttpResponse<null>) => {
-          switch (response.status) {
-            case 200:
-              this.onMaterialDeleted(material);
-              break;
-            default:
-              this.publishError(response);
-          }
-        },
-        (error: HttpErrorResponse) => this.publishError(error)
-      );
-  }
-
-  private loadInitialData(): void {
-    this.httpClient.get<MaterialType[]>(`${this.endpointUrl}`, { observe: 'response' })
-      .subscribe((response: HttpResponse<MaterialType[]>) => {
-          if (response.status == 200) this.materialsSubject.next(response.body);
-          else this.publishError(response);
-        },
-        (error: HttpErrorResponse) => this.publishError(error)
-      );
-  }
-
-  private onMaterialUpdated(updatedMaterial: MaterialType) {
-    this.materialsSubject.getValue().find((existentMaterial: MaterialType) => {
-      // Updates the name of the corresponding Material Type in the current list, and publish it
-      if (existentMaterial.id == updatedMaterial.id) {
-        existentMaterial.name = updatedMaterial.name;
-        this.materialsSubject.next(this.materialsSubject.getValue());
-        return true;
-      }
-    });
-  }
-
-  private onMaterialCreated(newMaterial: MaterialType) {
-    let materials: MaterialType[] = this.materialsSubject.getValue();
-    this.materialsSubject.next(materials.concat(newMaterial));
-  }
-
-  private onMaterialDeleted(deletedMaterial: MaterialType): void {
-    // Removes the deleted Material Type from the array
-    let materials: MaterialType[] = this.materialsSubject.getValue()
-      .filter((material: MaterialType) => material.id != deletedMaterial.id);
-
-    this.materialsSubject.next(materials);
-  }
-
-  private publishError(response: HttpResponseBase) {
-    this.errorSubject.next(ServiceError.fromStatusCode(response.status));
+    return this.subject.asObservable();
   }
 
 }
