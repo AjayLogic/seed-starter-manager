f439009c954f65170f838c4723d085aa469e871b
Eustácio <wallison.gth@gmail.com>
2017-12-28 07:12:38 -0200

    Refactored the Row interface to the new model class hierarchy

--- src/main/webapp/src/app/model/row.ts
+++ src/main/webapp/src/app/model/row.ts
@@ -1,7 +1,7 @@
+import { Entity } from './entity';
 import { SeedVariety } from './seed-variety';
 
-export interface Row {
-  id: number;
+export interface Row extends Entity {
   seedVariety: SeedVariety;
   seedsPerCell: number;
 }
