e80355b9ea9c3269b5ae1fd08c8c9025d2a2388b
Eustácio <wallison.gth@gmail.com>
2017-12-28 07:16:28 -0200

    Changed the SeedVariety class to a interface and refactor it to the new model class hierarchy. This closes #17

--- src/main/webapp/src/app/model/seed-variety.ts
+++ src/main/webapp/src/app/model/seed-variety.ts
@@ -1,6 +1,6 @@
-export class SeedVariety {
-  id: number;
-  name: string;
+import { NamedEntity } from './named-entity';
+import { SeedStarterFeature } from './seed-starter-feature';
+
+export interface SeedVariety extends NamedEntity, SeedStarterFeature {
   imageName?: File;
-  uses?: number;
 }
