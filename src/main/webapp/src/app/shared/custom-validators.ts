c33ae68207939b7b3c9873ebdaf88d4fcf32300b
Eustácio <wallison.gth@gmail.com>
2017-12-31 17:24:22 -0200

    Added a static method to the custom-validators to check if some entity has the property "name" equals to the text of the form control

--- src/main/webapp/src/app/shared/custom-validators.ts
+++ src/main/webapp/src/app/shared/custom-validators.ts
@@ -1,1 +1,18 @@
-export class CustomValidators {}
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+import { NamedEntity } from '../model/named-entity';
+
+export class CustomValidators {
+
+  static uniqueName(entities: NamedEntity[]): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      // Checks if some entity has the property 'name' equals to the text in the control.
+      let isDuplicated = entities.some((entity: NamedEntity) => {
+        return control.dirty && control.value === entity.name;
+      });
+
+      return isDuplicated ? { conflict: true } : null;
+    };
+  }
+
+}
